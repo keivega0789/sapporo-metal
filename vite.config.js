6ce422fdc1b1e7bebbe0dbd5974badb417dc1dff
keivega0789 <kei.s0789.180@gmail.com>
2025-11-16 15:32:53 +0900

    update: 会社情報ページ追加 / chore: vite設定変更

--- vite.config.js
+++ vite.config.js
@@ -1,4 +1,5 @@
 import { defineConfig } from "vite";
+import path from "path";
 
 export default defineConfig({
   base: process.env.NODE_ENV === "production" ? "/sapporo-metal/" : "/",
@@ -7,6 +8,12 @@ export default defineConfig({
   },
   build: {
     outDir: "docs",
+    rollupOptions: {
+      input: {
+        index: path.resolve(__dirname, "index.html"),
+        company: path.resolve(__dirname, "company.html"),
+      },
+    },
   },
   server: {
     open: true,
