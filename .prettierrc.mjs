615c2ccfb84dd881b53338dc4df05ffdf0c7788e
keivega0789 <kei.s0789.180@gmail.com>
2026-01-21 09:28:48 +0900

    refactor: prettier設定追加

---
+++ .prettierrc.mjs
@@ -0,0 +1,10 @@
+export default {
+  printWidth: 120,
+  tabWidth: 2,
+  semi: true,
+  singleQuote: true,
+  trailingComma: "es5", // ES5互換の位置のみ末尾カンマ（推奨）
+  bracketSpacing: true,
+  arrowParens: "avoid", // アロー関数の引数が1つなら括弧省略
+  endOfLine: "lf", // 改行コードをLFに統一
+};
